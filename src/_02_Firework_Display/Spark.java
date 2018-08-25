7654b513202013e4d1025c70d43954aa580c7519
Keith Groves <keithgroves@Keiths-MBP.attlocal.net>
2018-08-25 14:22:24 -0700

    Encapsulate Spark add getters and setters

--- src/_02_Firework_Display/Spark.java
+++ src/_02_Firework_Display/Spark.java
@@ -5,13 +5,13 @@ import java.awt.Graphics;
 import java.util.Random;
 
 public class Spark {
-	public int size = 5;
+	private int size = 5;
 	
-	public int x;
-	public int y;
+	private int x;
+	private int y;
 	public int xVelocity;
 	public int yVelocity;
-	public Color color;
+	private Color color;
 	
 	public boolean fire = false;
 	public boolean dead = false;
@@ -23,5 +23,53 @@ public class Spark {
 		color = new Color(r.nextInt(128) + 128, r.nextInt(128) + 128, r.nextInt(128) + 128);
 		yVelocity = -30;
 		xVelocity = 0;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public boolean isFire() {
+		return fire;
+	}
+
+	public void setFire(boolean fire) {
+		this.fire = fire;
+	}
+
+	public boolean isDead() {
+		return dead;
+	}
+
+	public void setDead(boolean dead) {
+		this.dead = dead;
 	}
 }
